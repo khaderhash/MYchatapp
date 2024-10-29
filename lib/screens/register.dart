acd8f18161cfd2c89553c848290824480f71bf7b
khader huijih <164516684+khaderGg@users.noreply.github.com>
2024-10-29 15:14:44 +0300

    'add'

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -22,62 +22,58 @@ class registerpage extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: ListView(children: [
             SizedBox(
-            height: 75,
-          ),
-
-                Image.asset('assets/photo/chat_app_logo.png', height: 150),
-                const Text(
-                  textAlign: TextAlign.center,
-                  "chat app",
-                  style: TextStyle(
-                      fontFamily: 'RobotoSlab',
-                      fontSize: 32,
-                      color: kPrimarycolor),
-                ),
+              height: 75,
+            ),
+            Image.asset('assets/photo/chat_app_logo.png', height: 150),
+            const Text(
+              textAlign: TextAlign.center,
+              "chat app",
+              style: TextStyle(
+                  fontFamily: 'RobotoSlab', fontSize: 32, color: kPrimarycolor),
+            ),
             SizedBox(
               height: 75,
             ),
-                const Text("Register page",
-                    style: TextStyle(color: kPrimarycolor, fontSize: 26)),
-                const SizedBox(
-                  height: 10,
-                ),
-                textfieldclass(
-                  hinttext: 'user name',
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                textfieldclass(
-                  hinttext: 'password',
-                ),
-                const SizedBox(
-                  height: 23,
-                ),
-                conclickclass(
-                  Texts: 'Register',
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("dont have an account? ",
-                        style: TextStyle(color: kPrimarycolor)),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        "Login",
-                        style: TextStyle(color: kPrimarycolor),
-                      ),
-                    )
-                  ],
-                ),
-
-              ]),
+            const Text("Register page",
+                style: TextStyle(color: kPrimarycolor, fontSize: 26)),
+            const SizedBox(
+              height: 10,
+            ),
+            textfieldclass(
+              hinttext: 'user name',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            textfieldclass(
+              hinttext: 'password',
+            ),
+            const SizedBox(
+              height: 23,
+            ),
+            conclickclass(
+              Texts: 'Register',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("dont have an account? ",
+                    style: TextStyle(color: kPrimarycolor)),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: kPrimarycolor),
+                  ),
+                )
+              ],
+            ),
+          ]),
         ),
       ),
     );
