3b1175de05daa5ff4f17ef97a3820d0128fe10d9
khader huijih <164516684+khaderGg@users.noreply.github.com>
2024-11-05 16:38:49 +0300

    firebaseee

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryth = Color(0xff28293F);
 const kPrimarycolor = Color(0xffFFFFFF);
-
+const messagecolletion = 'message';
 const KPphoto = 'assets/photo/chat_app_logo.png';
--- lib/screens/chatpage.dart
+++ lib/screens/chatpage.dart
@@ -1,49 +1,62 @@
 import 'package:chatapp/components/message.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../constants.dart';
 
 class Chatpage extends StatelessWidget {
-  const Chatpage({super.key});
+  Chatpage({super.key});
   static String id = "chatpage";
-
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(messagecolletion);
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          // centerTitle: true,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                KPphoto,
-                height: 50,
+    return FutureBuilder<DocumentSnapshot>(
+      future: messages.doc('0VjyLQIUeURihp9QxVac').get(),
+      builder: (context, snapshot) {
+        print(snapshot.data!['message']);
+        return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              // centerTitle: true,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    KPphoto,
+                    height: 50,
+                  ),
+                  Text("chat")
+                ],
               ),
-              Text("chat")
-            ],
-          ),
-          backgroundColor: kPrimaryth,
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return messagebuble();
-                },
-              ),
+              backgroundColor: kPrimaryth,
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.send, color: kPrimaryth),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20))),
-              ),
-            )
-          ],
-        ));
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return messagebuble();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (value) {
+                      messages.add({'message': value});
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.send, color: kPrimaryth),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                )
+              ],
+            ));
+      },
+    );
   }
 }
